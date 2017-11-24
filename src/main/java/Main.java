c4b1ddca0cbc5c7936da4f37c2493d7a87db998c
Madeleine Barwén <barwen@outlook.com>
2017-11-24 11:04:42 +0100

    Adding tests to method add, example on delta in Main etc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,25 @@
 import java.util.Random;
 
 /**
- * Main class. 
+ * Main class.
  **/
 
 public class Main {
-	public static void main(String[] args){
-		Calculator cal = new Calculator();
-		Random ran = new Random();
-		
-		int firstSmallPosNumber = ran.nextInt(11);
-		int secondSmallPosNumber = ran.nextInt(11);
-		
-		cal.add(firstSmallPosNumber, secondSmallPosNumber);
-		
-		//System.out.println(cal.add(firstNumber, secondNumber));
+	public static void main(String[] args) {
+		// Calculator cal = new Calculator();
+		// Random ran = new Random();
+		//
+		// double firstSmallPosNumber = ran.nextDouble();
+		// double secondSmallPosNumber = ran.nextDouble();
+		//
+		// cal.add(firstSmallPosNumber, secondSmallPosNumber);
+
+		// System.out.println(cal.add(firstNumber, secondNumber));
+
+		// För att förklara varför vi använder delta 1 vid decimaler som vi
+		// avrundar.
+//		System.out.println(3.5 * Math.PI);
+//		System.out.println(3.5 * 3.14);
 	}
 
 }
